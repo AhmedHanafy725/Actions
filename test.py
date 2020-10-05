f92611c64ff6aa31358c65aca0e9c87783c19374
AhmedHanafy725 <41957921+AhmedHanafy725@users.noreply.github.com>
2020-10-05 21:50:19 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def test():
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("ignore-certificate-errors")
+    self.driver = webdriver.Chrome(options=options)
+    self.driver.implicitly_wait(10)
+    self.driver.maximize_window()
+    self.driver.get("https://www.google.com/)
+    
